7e9cd154d119f3b7332ef74b6e1780aec0f5feac
Timam <mail@timam.io>
2023-10-22 02:29:41 +0600

    added log

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World!")
+		_, err := fmt.Fprint(w, "Genie says Hello World!")
+		if err != nil {
+			return
+		}
+		log.Println("responded with hello world")
 	})
 	http.ListenAndServe(":8081", nil)
 }
